12dec8da6ddcf5375b32a1bc2b1e1d4023ef6503
HB-Leang <bunleang5289@gmail.com>
2025-03-04 14:59:15 +0700

    first commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        Dashboard by
+        <a href="#" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">The Secret Organization</a>
+    </div>
+</template>
